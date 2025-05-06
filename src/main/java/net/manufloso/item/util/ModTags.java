e57a1a37d0ea35388b0e3707be17afd9b6155c2c
ManuFlosoYT <manubh94y@gmail.com>
2025-05-06 15:21:30 +0200

    tags

---
+++ src/main/java/net/manufloso/item/util/ModTags.java
@@ -0,0 +1,30 @@
+package net.manufloso.item.util;
+
+import net.manufloso.supermodreborn;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags
+{
+    public static class Blocks
+    {
+        private static TagKey<Block> createTag(String name)
+        {
+            return BlockTags.create(ResourceLocation.fromNamespaceAndPath(supermodreborn.MODID, name));
+        }
+    }
+
+    public static class Items
+    {
+        public static final TagKey<Item> LITHIUM = createTag("lithium");
+
+        private static TagKey<Item> createTag(String name)
+        {
+            return ItemTags.create(ResourceLocation.fromNamespaceAndPath(supermodreborn.MODID, name));
+        }
+    }
+}
